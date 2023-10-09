9f609e513198bb2863de3d6fad6fc16521dea3f7
AugustoM94 <email@github.com>
2023-10-09 17:34:50 +0200

    added if on script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). 
 Questo richiederà un minimo di ricerca. */
+
+let km = parseInt(prompt ("Inserisci il numero di km da percorrere:"));
+let age = parseInt(prompt ("Inserisci l'età del passeggero:"));
+let tariffa = 0.21;
+
+if ('age' < 18) {
+  'Prezzo_totale_biglietto' = 'km' * ('tariffa' - ('tariffa' * 20/100));
+} else if ('age' > 65) {
+    'Prezzo_totale_biglietto' = 'km' * ('tariffa' - ('tariffa' * 40/100));
+} else {
+    'Prezzo_totale_biglietto' = ('km' * 'tariffa');
+}
+let Prezzo_totale_biglietto = `
+
+<p> 'Prezzo_totale_biglietto' </p>
+
+`
+document.getElementById('title').innerHTML = Prezzo_totale_biglietto
